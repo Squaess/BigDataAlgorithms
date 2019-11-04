1bfa7a5a429ca6cb07f86e3c6655a4d1e485fd98
Bartosz Banasik <babartosz.bns@gmail.com>
2019-11-04 23:21:50 +0100

    Frequencies and inverse are calculated, need to calculate TF-IDF.

--- problem1/src/main/scala/problem2/Problem2.scala
+++ problem1/src/main/scala/problem2/Problem2.scala
@@ -8,15 +8,32 @@ object Problem2 extends App {
     val documents:Array[Array[String]] = Source.fromResource("book.txt")(Codec("UTF-8"))
       .getLines
       .mkString
-      .toLowerCase
       .replaceAll(",|\\.|!|\\?", " ")
-      .split("rozdział")
+      .split("Rozdział (I|V|X|L|C|D|M)*")
       .drop(1)
       .map(_
         .strip
+        .toLowerCase
         .split("\\s+")
         .filterNot(stopwords_pl.contains(_))
       )
 
-    documents.head.take(10).foreach(println)
+    val frequency:Array[Map[String, Double]] = documents
+      .map( x => (x, x.length))
+      .map( x => x._1
+        .toList
+        .groupBy(x => x)
+        .mapValues(y => y.length/x._2.toDouble),
+    )
+
+    val inverse:Map[String, Double] = frequency
+      .flatMap(_.toList)
+      .groupBy(x => x._1)
+      .mapValues(x => x.length/documents.length.toDouble)
+
+    frequency.map(
+        x => x.map(
+            y => (y._1, y._2 * inverse.getOrElse(y._1, 0))
+        )
+    ).head.take(10).foreach(println)
 }
