a05399830db55cff61bc6d49f832be2e14706b57
Bartosz Banasik <babartosz.bns@gmail.com>
2019-11-14 12:58:56 +0100

    I thing it's better now

--- ObjectComparison/src/main/scala/example/JaccardSimilarity.scala
+++ ObjectComparison/src/main/scala/example/JaccardSimilarity.scala
@@ -35,31 +35,55 @@ class JaccardSimilarity(File1: String, File2: String, k: Int, h: Int){
         (set1 intersect set2).size / (set1 union set2).size.toDouble
     }
 
-    override def toString(): String = s"${jaccard_sim(text1_shingle, text2_shingle)}"
 
 
     val h1: MurmurHash3.type = MurmurHash3
     // List of all distinct words
     val rows = (text1.split("\\s+").toSet union text2.split("\\s+").toSet).toSeq.sorted
-    var minHash1: Array[Int] = Array.fill(h)(Int.MaxValue)
-    var minHash2: Array[Int] = Array.fill(h)(Int.MaxValue)
+    // var minHash1: Array[Int] = Array.fill(h)(Int.MaxValue)
+    // var minHash2: Array[Int] = Array.fill(h)(Int.MaxValue)
     val text1_splited = text1.split("\\s+").toSet
     val text2_splited = text2.split("\\s+").toSet
-    for (i <- 0 until h){
-        //calculate has values fo the rows for the i-th hash function
-        val hashed1 = rows.map(
-            x => if ( h1.stringHash(x, i) % rows.size >= 0 ) h1.stringHash(x, i) % rows.size
-                    else h1.stringHash(x, i) % rows.size + rows.size)
-        // get indexes from rows, fot the values of text1 and text2
-        val indexes1 = for (w: String<- text1_splited) yield rows.indexOf(w)
-        val indexes2 = for (w: String<- text2_splited) yield rows.indexOf(w)
+    // for (i <- 0 until h){
+    //     //calculate has values fo the rows for the i-th hash function
+    //     val hashed1 = rows.map(
+    //         x => if ( h1.stringHash(x, i) % rows.size >= 0 ) h1.stringHash(x, i) % rows.size
+    //                 else h1.stringHash(x, i) % rows.size + rows.size)
+    //     // get indexes from rows, fot the values of text1 and text2
+    //     val indexes1 = for (w: String<- text1_splited) yield rows.indexOf(w)
+    //     val indexes2 = for (w: String<- text2_splited) yield rows.indexOf(w)
 
-        minHash1(i) = hashed1.zipWithIndex.filter(x => indexes1 contains x._2).minBy(x => x._1)._1
-        minHash2(i) = hashed1.zipWithIndex.filter(x => indexes2 contains x._2).minBy(x => x._1)._1
-    }
-    val minhashed_value = (minHash1 zip minHash2).filter( x => x._1 == x._2).length / minHash1.length.toDouble
+    //     minHash1(i) = hashed1.zipWithIndex.filter(x => indexes1 contains x._2).minBy(x => x._1)._1
+    //     minHash2(i) = hashed1.zipWithIndex.filter(x => indexes2 contains x._2).minBy(x => x._1)._1
+    // }
+    // val minhashed_value = (minHash1 zip minHash2).filter( x => x._1 == x._2).length / minHash1.length.toDouble
     // println(hashed1.zipWithIndex.filter(x => indexes contains x._2))
     // println(hashed1.take(100))
     // println(rows.size)
     // println(rows.take(100))
+
+    def genHashes(h: Int, rows: Seq[String]) = {
+        (0 until h).map(
+            x => x -> rows.map(
+            y => if ( h1.stringHash(y, x) % rows.size >= 0 ) h1.stringHash(y, x) % rows.size
+                    else h1.stringHash(y, x) % rows.size + rows.size
+            )
+        )
+    }
+
+    def getSignature(indexes: Seq[Int], hashes: IndexedSeq[(Int, Seq[Int])]): Seq[Int] = {
+        hashes.map( x =>
+            x._2.filter( y => indexes contains x._2.indexOf(y)).min
+        )
+    }
+
+    val hashes: IndexedSeq[(Int, Seq[Int])] = genHashes(h, rows)
+    val minHash1 = getSignature(text1_splited.map(x => rows.indexOf(x)).toSeq, hashes)
+    val minHash2 = getSignature(text2_splited.map(x => rows.indexOf(x)).toSeq, hashes)
+    val minhashed_value = (minHash1 zip minHash2).filter( x => x._1 == x._2).length / minHash1.length.toDouble
+
+    override def toString(): String = {
+        s"Jaccard shignle: ${jaccard_sim(text1_shingle, text2_shingle)}" + "\n" +
+        s"MinHash for h=$h: $minhashed_value"
+    }
 }
